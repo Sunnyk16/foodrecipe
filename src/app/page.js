7acbc59320f26ff30a9d90135a6b2840c5ac5b6a
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-15 17:37:45 +0530

    ref:added final css

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+// import img1 from "../../public/newsstand.gif";
 
 export default function Home() {
   return (
     <main>
-      <div className="text-center capitalize font-bold text-5xl  ">
+      <div className="text-center capitalize font-bold text-5xl bg-[url('https://storyset.com/illustration/witch/bro')]  h-screen bg-cover">
+        {/* <img src={'https://img.freepik.com/free-photo/top-view-bowls-with-indian-food_23-2148723454.jpg?w=996&t=st=1723723046~exp=1723723646~hmac=250b5d42abb300566313c1c8cd351b77854facdae4b4d6c5edc39aa1f6e97a6b'} alt="newsstand" className=" " /> */}
         welcome to recipes
+        <div>
+        <Link href={"/recipe-list"}>
+          <button className="border border-emerald-500 capitalize p-2 m-2 rounded-md hover:translate-y-1 hover: bg-emerald-400 hover:text-white ">
+            explore
+          </button>
+        </Link>
+        </div>
       </div>
-      <Link href={"/recipe-list"}>
-        <button className="border border-emerald-500 capitalize p-2 m-2 rounded-md hover:translate-y-1 hover: bg-emerald-400 hover:text-white ">
-          explore
-        </button>
-      </Link>
     </main>
   );
 }
--- src/app/recipe-list/[details]/page.js
+++ src/app/recipe-list/[details]/page.js
@@ -5,7 +5,7 @@
 //     const apiresponse = await fetch(`https://dummyjson.com/recipes/${_currentRecipeId}`);
 //     const data =await apiresponse.json();
 //     return data;
-    
+
 //   } catch (error) {
 //     throw new Error(error);
 //   }
@@ -15,7 +15,7 @@
 // async function RecipeDetails({params}) {
 //   const getRecipeDetails = await fetchRecipeDetails(params?.details);
 //   console.log(getRecipeDetails);
-  
+
 //   return (
 //     <div>
 //       <RecipeDetailsItems getRecipeDetails={getRecipeDetails}/>
@@ -52,35 +52,39 @@ async function RecipeDetails({ params }) {
   console.log(getRecipeDetails);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">This is recipe details page</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {/* Left Side - Image */}
-        <div className="flex justify-center items-center">
-          {getRecipeDetails?.image && (
-            <img
-              src={getRecipeDetails.image}
-              alt={getRecipeDetails?.name}
-              className="max-w-full h-auto rounded shadow-lg"
-            />
-          )}
-        </div>
-
-        {/* Right Side - Details */}
-        <div className="flex flex-col justify-center">
-          <p className="text-lg font-semibold mb-2">
-            Meal Type: {getRecipeDetails?.mealType}
-          </p>
-          <div className="text-lg font-semibold mb-2">
-            Cuisine: {getRecipeDetails?.cusine}
+    <div className="min-h-screen  flex justify-center items-center">
+      <div className="container mx-auto p-4 border-2 shadow-lg drop-shadow-lg border-emerald-400 hover:scale-105 transition-transform">
+        <h1 className="md:text-6xl capitalize text-emerald-500 font-bold mb-4 text-center sm:text-4xl">
+          {getRecipeDetails?.name}
+        </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Left Side - Image */}
+          <div className="flex justify-center items-center">
+            {getRecipeDetails?.image && (
+              <img
+                src={getRecipeDetails.image}
+                alt={getRecipeDetails?.name}
+                className="max-w-96 h-auto rounded-lg shadow-lg "
+              />
+            )}
           </div>
-          <div className="text-lg font-semibold">
-            Ingredients:{" "}
-            <ul className="list-disc list-inside">
-              {getRecipeDetails?.ingredients?.map((ingredient, index) => (
-                <li key={index}>{ingredient}</li>
-              ))}
-            </ul>
+
+          {/* Right Side - Details */}
+          <div className="flex flex-col justify-center">
+            <p className="text-lg  font-semibold mb-2">
+              <span className="text-emerald-400">Meal Type</span>: {getRecipeDetails?.mealType}
+            </p>
+            <div className="text-lg font-semibold mb-2">
+              {/* Cuisine: {getRecipeDetails?.cusine} */}
+            </div>
+            <div className="text-lg font-semibold">
+              <span className="text-emerald-400">Ingredients</span>:{" "}
+              <ul className="list-disc list-inside">
+                {getRecipeDetails?.ingredients?.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
